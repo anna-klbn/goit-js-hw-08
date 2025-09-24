d5f93f20291dfcb30bdb42e1f470bc4e8b9ccc44
anya-kashchenko <143807908+anna-klbn@users.noreply.github.com>
2025-09-24 19:12:00 +0300

    task-2,3

---
+++ src/js/02-video.js
@@ -0,0 +1,15 @@
+import throttle from 'lodash.throttle';
+
+import Player from '@vimeo/player';
+
+const VIDEOPLAYER_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function (event) {
+  localStorage.setItem(VIDEOPLAYER_KEY, event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(VIDEOPLAYER_KEY)) || 0);
